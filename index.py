7e5966e5385968448ed81a530f714ca05dedf37c
LF3551 <adk3551@gmail.com>
2022-04-11 23:11:30 +0300

    guide how to create serverless bot

--- index.py
+++ index.py
@@ -4,10 +4,7 @@ from aiogram import Bot, Dispatcher, types as aiotypes
 TOKEN = os.getenv("TOKEN")
 
 async def process_event(event, dp: Dispatcher):
-    print(event['body'])
-    print(event)
     update = json.loads(event['body'])
-    print("Got TG update, started processing")
     Bot.set_current(dp.bot)
     update = aiotypes.Update.to_object(update)
     await dp.process_update(update)
@@ -17,7 +14,7 @@ async def handler(event, context):
     bot = Bot(TOKEN)
     dp = Dispatcher(bot)
     @dp.message_handler(commands=['start'])
-    async def get_voice(message: aiotypes.Message):
-        await message.reply("Не получилось распознать")
+    async def send_message(message: aiotypes.Message):
+        await message.reply("Hello user")
     await process_event(event, dp)
     return {'statusCode': 200, 'body': 'ok'}
